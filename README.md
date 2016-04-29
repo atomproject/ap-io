6419eef1f80821f48904fc824410b407d788e471
Ajinkya Tejankar <tejankarajinkya@gmail.com>
2016-04-29 17:06:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Documentation generator
+
+Interactive documentation generator for polymer elements
+
+# Deps
+
+1. Node (>6.0.0)
+2. Npm (>3.8.6)
+
+# Installation
+
+npm i -g atom-doc-generator
---
+++ browser/build.js
@@ -0,0 +1,129 @@
+'use strict';
+
+let fs = require('fs');
+let path = require('path');
+let fm = require('front-matter');
+let Liquid = require('liquid-node');
+let mkdirp = require('mkdirp');
+let marked = require('marked');
+let promisify = require('promisify-node');
+let glob = promisify(require('glob'));
+let engine = new Liquid.Engine();
+let getFullConfig = require('./config').getFullConfig;
+
+function handleError(err) {
+  console.log(err.stack || err);
+  process.exit(1);
+}
+
+function resolveLayout(filePath, config,queue) {
+  let file = fs.readFileSync(path.resolve(filePath), 'utf-8');
+  let layout;
+
+  file = fm(file);
+  queue = queue || [];
+  queue.push(file);
+
+  if (file.attributes && (layout = file.attributes.layout)) {
+    filePath = `${config.layoutsDir}/${layout}.html`;
+    resolveLayout(filePath, config, queue);
+  }
+
+  return queue;
+}
+
+function renderLayout(queue, context) {
+  let p = Promise.resolve('');
+
+  queue.forEach(item => {
+    Object.assign(context.page, item.attributes);
+
+    p = p.then(content => {
+      context.content = content;
+
+      return engine.parseAndRender(item.body, context);
+    });
+  });
+
+  return p;
+}
+
+function createElementPage(elContext, config) {
+  let queue = resolveLayout(`${config.templatesDir}/github.html`, config);
+  let fullContext = {
+    site: config,
+    page: elContext
+  };
+
+  renderLayout(queue, fullContext)
+    .then(page => {
+      let pagePath = elContext.pageDirName;
+
+      mkdirp.sync(path.join('_site', pagePath));
+      pagePath = path.join('_site', pagePath ,'index.html');
+
+      console.log(`Build: ${pagePath}`);
+      fs.writeFileSync(pagePath, page);
+    })
+    .catch(handleError);
+}
+
+function createPage(filePath, config) {
+  if (!fs.statSync(filePath).isFile()) {
+    return;
+  }
+
+  let queue = resolveLayout(filePath, config);
+  let pathObj = path.parse(filePath);
+  let context = {site: config, page: {}};
+
+  if (config.markdownExtensions.indexOf(pathObj.ext) !== -1) {
+    queue[0].body = marked(queue[0].body || '');
+  }
+
+  renderLayout(queue, context)
+    .then(page => {
+      let pagesDir = path.resolve(config.pagesDir);
+      let outDir = path.resolve('_site');
+      let pathObj = path.parse(filePath);
+
+      outDir = path.resolve(pathObj.dir).replace(pagesDir, outDir);
+
+      if (pathObj.name !== 'index') {
+        outDir = path.join(outDir, pathObj.name);
+      }
+
+      mkdirp.sync(outDir);
+      let pagePath = path.join(outDir, `index.html`);
+
+      console.log(`Build: ${pagePath}`);
+      fs.writeFileSync(pagePath, page);
+    })
+    .catch(handleError);
+}
+
+module.exports = function build() {
+  getFullConfig().then(config => {
+    //setup yaml parser engine
+    engine.fileSystem = new Liquid.LocalFileSystem();
+    engine.fileSystem.root = config.includesDir;
+
+    //create the out dir
+    mkdirp.sync('_site');
+
+    //create element pages
+    config.elements.forEach(elContext => {
+      createElementPage(elContext, config);
+    });
+
+    //create other pages
+    glob(`${config.pagesDir}/**`)
+      .then(files => {
+        files.forEach(filePath => {
+          createPage(filePath, config);
+        });
+      })
+      .catch(handleError);
+
+  }).catch(handleError);
+};
